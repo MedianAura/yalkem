d9b371871bafc9cadd86c482085ea7e79dad8795
Sebastien Lafleur <sebastien.lafleur@gmail.com>
2024-06-11 15:16:03 -0400

    feature: add local package support
    add local package support
    
    [log]

--- src/helpers/local-configuration.ts
+++ src/helpers/local-configuration.ts
@@ -1,8 +1,24 @@
-import Conf from 'conf';
-import type { ConfigurationModelSchema } from '../models/configuration.js';
+import path from 'node:path';
+import { readFileSync, writeFileSync } from 'node:fs';
 
-const config = new Conf<ConfigurationModelSchema>({ projectName: 'yalkem' });
+const localConfigurationPath = path.resolve(process.cwd(), '.yalkemrc');
 
-export function getConfiguration(): Conf<ConfigurationModelSchema> {
-  return config;
+function readConfigurationFile(): Record<string, boolean> {
+  try {
+    const configuration = readFileSync(localConfigurationPath, { encoding: 'utf8' });
+    return JSON.parse(configuration);
+  } catch {
+    return {};
+  }
+}
+
+export function getLocalConfiguration(): Record<string, boolean> {
+  return readConfigurationFile();
+}
+
+export function updateLocalConfiguration(configuration: Record<string, boolean>): void {
+  const currentConfiguration = getLocalConfiguration();
+  const configurationString = JSON.stringify({ ...currentConfiguration, ...configuration }, undefined, 2);
+
+  writeFileSync(localConfigurationPath, configurationString, { encoding: 'utf8' });
 }
